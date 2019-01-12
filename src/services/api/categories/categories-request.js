d751a61cfdefee5545bc8a754aca00b729cc1b22
CMOW5 <tec.cmos@gmail.com>
2019-01-12 00:46:28 -0500

    count method fixed

--- src/services/api/categories/categories-request.js
+++ src/services/api/categories/categories-request.js
@@ -25,7 +25,7 @@ export default class CategoriesRequest {
     return new Promise((resolve, reject) => {
       httpRequester.get(url)
         .then((response) => {
-          const count = response.data.data;
+          const count = response.data.content || 0;
           resolve(count);
         })
         .catch((error) => {
