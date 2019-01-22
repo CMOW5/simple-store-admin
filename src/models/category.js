b36df3eb322e531e3dea278feab3eae1a226ae6b
CMOW5 <tec.cmos@gmail.com>
2019-01-21 23:44:05 -0500

    a constructor was added to initialze the category with proper data

--- src/models/category.js
+++ src/models/category.js
@@ -7,11 +7,17 @@ export default class Category {
    *
    * @param {object} initialCategoryData
    */
-  constructor(initialCategoryData) {
+  constructor(initialCategoryData = {}) {
     this.id = initialCategoryData.id || '';
     this.name = initialCategoryData.name || '';
-    this.parentCategory = initialCategoryData.parentCategory || '';
+    /*
+      TODO: this {id: null} 'hack' was created to avoid
+      errors when the parentCategory = null, so null.id
+      throws a TypeError
+    */
+    this.parentCategory = initialCategoryData.parentCategory || {id: null};
     this.products = initialCategoryData.products || [];
     this.subcategories = initialCategoryData.subcategories || [];
+    this.image = initialCategoryData.image || '';
   }
 }
