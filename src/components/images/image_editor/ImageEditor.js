b0a016be24093f1d0eddb4d6ae0895b38ba31a25
CMOW5 <tec.cmos@gmail.com>
2019-01-27 20:06:22 -0500

    the component now renders a SingleImageEditor or a MultipleImageEditor depending of the singleImage prop

--- src/components/images/image_editor/ImageEditor.js
+++ src/components/images/image_editor/ImageEditor.js
@@ -1,182 +1,79 @@
-import React, {Component} from 'react';
-import SingleImageView from './utils/SingleImageView';
+import React, {Component, Fragment} from 'react';
 
+// styles
 import './image-editor.css';
 
+// components
+import MultipleImageEditor from './MultipleImageEditor';
+import SingleImageEditor from './SingleImageEditor';
+
 /**
- * component to select images from local disk
+ * this component is used to select images from local disk
  */
 export default class ImageEditor extends Component {
   /**
-   * @param {Object} props
+   * @param {object} props
    */
   constructor(props) {
     super(props);
-    this.state = {
-      files: props.initImages ? [...props.initImages] : [],
-      idsToDelete: [],
-    };
-    this.removeFile = this.removeFile.bind(this);
-    this.handleFilesUpload = this.handleFilesUpload.bind(this);
-    this.notifyChangeToParent = this.notifyChangeToParent.bind(this);
-    this.appendFiles = this.appendFiles.bind(this);
+    this.renderEditor = this.renderEditor.bind(this);
+    this.handleImagesLoadedMultiple
+      = this.handleImagesLoadedMultiple.bind(this);
+    this.handleImagesLoadedSingle
+      = this.handleImagesLoadedSingle.bind(this);
   }
 
   /**
-   * init some component data
-   */
-  componentDidMount() {
-    if (!this.props.singleImage) {
-      this.$files.setAttribute('multiple', '');
-    }
-  }
-
-  /**
-   * determines if the component should update after the
-   * props have changed
    *
-   * @param {*} prevProps
-   * @param {*} prevState
+   * @param {*} uploadedImages
+   * @param {*} idsToDelete
    */
-  componentDidUpdate(prevProps, prevState) {
-    if (prevProps.initImages !== this.props.initImages) {
-      this.appendFiles(this.props.initImages);
-    }
+  handleImagesLoadedMultiple(uploadedImages, idsToDelete) {
+    this.props.onImagesLoaded(uploadedImages, idsToDelete);
   }
 
   /**
-   * load the images urls passed from the parent
-   * in the images view
-   * @param {array} files
+   * convert the SingleImageEditor event values
+   * to arrays to make the event values consistent
+   * with MultipleImageEditor
+   * @param {object} uploadedImages
+   * @param {number} idsToDelete
    */
-  async appendFiles(files) {
-    if (!files) return;
-
-    this.setState((prevState) => ({
-      files: [...prevState.files, ...files],
-    }));
+  handleImagesLoadedSingle(uploadedImages, idsToDelete) {
+    const uploadedImagesArray = uploadedImages ? [uploadedImages] : null;
+    const idsToDeleteArray = idsToDelete ? [idsToDelete] : null;
+    this.props.onImagesLoaded(uploadedImagesArray, idsToDeleteArray);
   }
 
   /**
-   * remove the selected file
-   * @param {File} fileToRemove
+   * @return {ReactNode}
    */
-  removeFile(fileToRemove) {
-    // add the file id to idsToDelete if the selected file
-    // was passed through props
-    let idsToDelete = this.state.idsToDelete;
-
-    if (fileToRemove.constructor !== File) {
-      const idToDelete =
-        this.state.files
-          .filter((file) => {
-            return fileToRemove === file.url;
-          })
-          .map((file) => {
-            return file.id;
-          });
-      idsToDelete = [...idsToDelete, ...idToDelete];
-    }
-
-    // remove the file and save the remainig files
-    // in a new array
-    const newFiles = this.state.files.filter((file) => {
-      if (file.constructor === File) {
-        return file !== fileToRemove;
-      } else {
-        return file.url !== fileToRemove;
-      }
-    });
-
-    this.setState((prevState) => ({
-      files: newFiles,
-      idsToDelete: idsToDelete,
-    }), this.notifyChangeToParent);
-  }
-
-  /**
-   * notify to the parent the changes in the selected images
-   */
-  notifyChangeToParent() {
-    // filter the new images from the original images
-    // passed from the parent component
-    const newFiles = this.state.files.filter((file) => {
-      return file.constructor === File;
-    });
-    this.props.onImagesLoaded(newFiles, this.state.idsToDelete);
-  }
-
-  /**
-   * this event is triggered once the user has selected
-   * the images
-   */
-  handleFilesUpload() {
-    let newFiles;
-
+  renderEditor() {
     if (this.props.singleImage) {
-      // choose only the selected file
-      newFiles = [...this.$files.files];
+      return (
+        <SingleImageEditor
+          initImage={this.props.initImages}
+          onImagesLoaded={this.handleImagesLoadedSingle}
+        />
+      );
     } else {
-      // concatenate the current files with the files from the input.
-      newFiles = [...this.state.files, ...this.$files.files];
+      return (
+        <MultipleImageEditor
+          initImages={this.props.initImages}
+          onImagesLoaded={this.handleImagesLoadedMultiple}
+        />
+      );
     }
-
-    this.setState((prevState) => ({
-      files: newFiles,
-    }), this.notifyChangeToParent);
   }
 
   /**
    *  @return {ReactNode}
    */
   render() {
-    // jsx
-    const images = this.state.files.map((file, index) => {
-      const image = file.constructor === File ? file : file.url;
-      return (
-        // Card to show each image
-        <div className="column is-2" key={index}>
-          <SingleImageView
-            file={image}
-            onImageRemoved={this.removeFile}>
-          </SingleImageView>
-        </div>
-      );
-    });
-
     return (
-      <div className="image-picker">
-
-        {/* Input element that is hidden for styles purposes */}
-        <div className="file">
-          <label className="file-label">
-            <input
-              className="file-input"
-              type="file"
-              name="resume"
-              ref={(input) => {
-                this.$files = input;
-              }}
-              accept="image/*"
-              onChange={this.handleFilesUpload}
-            />
-            <span className="file-cta">
-              <span className="file-icon">
-                <i className="fa fa-upload"></i>
-              </span>
-              <span className="file-label">
-                Choose a file…
-              </span>
-            </span>
-          </label>
-        </div>
-
-        {/* Preview the images loaded */}
-        <div className="columns is-multiline image-cards">
-          {images}
-        </div>
-
-      </div>
+      <Fragment>
+        {this.renderEditor()}
+      </Fragment>
     );
   }
 }
