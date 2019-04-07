879a327bccbad93460c34fc1836d675b19319aa3
bit5 <tec.cmos@gmail.com>
2019-04-06 21:38:47 -0500

    root reducer has been moved to its own file

--- src/store/store.js
+++ src/store/store.js
@@ -1,30 +1,24 @@
-// import {createStore, combineReducers, applyMiddleware} from 'redux';
-import {createStore, combineReducers, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 
 /* middlewares */
-import reduxThunk from 'redux-thunk';
+import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 // import promise from 'redux-promise-middleware';
 
 // reducers
-import userReducer from './reducers/user-reducer';
+import rootReducer from './reducers/root-reducer';
 
-const createStoreWithMiddleware
-  = applyMiddleware(
-    reduxThunk, // async state calls
-    logger, // log the state changes
-  )(createStore);
-
-const rootReducer = combineReducers(
-  {
-    userReducer,
-  });
+const createStoreWithMiddleware = applyMiddleware(
+  thunk, // async state calls
+  logger, // log the state changes
+)(createStore);
 
 export default createStoreWithMiddleware(
   rootReducer,
   {},
   // applyMiddleware(logger, thunk, promise())
-  // redux dev tools
+
+  // enable redux dev tools
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
