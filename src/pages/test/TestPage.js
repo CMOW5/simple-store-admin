21e7e7d1cac8a666ddecc95e440d44759c8c73dc
bit5 <tec.cmos@gmail.com>
2019-04-06 20:01:49 -0500

    test page

--- src/pages/test/TestPage.js
+++ src/pages/test/TestPage.js
@@ -18,19 +18,9 @@ export default class TestPage extends Component {
    * @return {ReactNode}
    */
   render() {
+    console.log('Testpaged');
     return (
-      <di>TEST PAGE</di>
+      <div>TEST PAGE</div>
     );
   }
 }
-
-// The following code is based off a toggle menu by @Bradcomp
-// source: https://gist.github.com/Bradcomp/a9ef2ef322a8e8017443b626208999c1
-/* (function() {
-  var burger = document.querySelector('.burger');
-  var menu = document.querySelector('#'+burger.dataset.target);
-  burger.addEventListener('click', function() {
-      burger.classList.toggle('is-active');
-      menu.classList.toggle('is-active');
-  });
-})(); */
