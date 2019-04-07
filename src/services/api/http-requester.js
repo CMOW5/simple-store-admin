c278fd615a13e8c45dc2099cfcc49b839876c014
bit5 <tec.cmos@gmail.com>
2019-04-06 23:34:48 -0500

    static class changed to plain js object

--- src/services/api/http-requester.js
+++ src/services/api/http-requester.js
@@ -4,165 +4,116 @@ import axios from 'axios';
 import Logger from 'utils/logger/logger';
 import TokenHandler from './auth/utils/token-handler';
 
+export default {
+  submit: submit,
+  get: getRequest,
+  post: postRequest,
+  put: putRequest,
+  patch: patchRequest,
+  delete: deleteRequest,
+};
+
 /**
- * helper class to make http requests
+ *  @param {string} requestType 'GET, PUT, POST or PATCH'
+ *  @param {string} url
+ *  @param {object} data
+ *  @return {Promise}
  */
-export default class HttpRequester {
-  /**
-   * this class name.
-   *
-   * @return {string} the class name
-   */
-  static className() {
-    return 'HttpRequester';
-  }
+export function submit(requestType, url, data = {}) {
+  Logger.log('HttpRequest submit requestType= ' + requestType +
+      ' url= ' + url + ' data= ', data);
 
-  /**
-   * Send a GET request to the given URL.
-   *
-   * @param {string} url
-   * @return {Promise}
-   */
-  static get(url) {
-    return this.submit('get', url);
-  }
-
-  /**
-   * Send a POST request to the given URL.
-   *
-   * @param {string} url the request url
-   * @param {Object} data the data attached to the request
-   * @return {Promise}
-   */
-  static post(url, data={}) {
-    return this.submit('post', url, data);
-  }
-
-  /**
-   * Send a PUT request to the given URL.
-   *
-   * @param {string} url the request url
-   * @param {Object} data the data attached to the request
-   * @return {Promise}
-   */
-  static put(url, data={}) {
-    return this.submit('put', url, data);
-  }
-
-  /**
-   * Send a PATCH request to the given URL.
-   *
-   * @param {string} url the request url
-   * @param {Object} data the data attached to the request
-   * @return {Promise}
-   */
-  static patch(url, data={}) {
-    return this.submit('patch', url, data);
-  }
-
-  /**
-   * Send a DELETE request to the given URL.
-   *
-   * @param {string} url the request url
-   * @return {Promise}
-   */
-  static delete(url) {
-    return this.submit('delete', url);
-  }
-
-  /**
-   * Submit the data.
-   *
-   * @param {string} requestType
-   * @param {string} url
-   * @param {Object} data
-   * @return {Promise}
-   */
-  static submit(requestType, url, data = {}) {
-    const methodName = ' submit(..) ';
-
-    Logger.log(this.className() + methodName +
-      'requestType= ' + requestType + ' url= ' + url + ' data= ', data);
-
-    let headers = {
-      // 'Accept': 'application/json',
-      'Content-type': 'application/json',
-      // "Authorization": "Bearer " + sessionStorage.getItem('jwt')
-    };
+  let headers = {
+    // 'Accept': 'application/json',
+    'Content-type': 'application/json',
+  };
 
     // get the token first
-    const token = TokenHandler.getToken();
+  const token = TokenHandler.getToken();
 
-    if (token) {
-      headers['Authorization'] = 'Bearer ' + token;
-    }
+  if (token) {
+    headers['Authorization'] = 'Bearer ' + token;
+  }
 
+  const config = {
+    method: requestType,
+    url: url,
+    data: data,
+    headers: headers,
+  };
 
-    return new Promise((resolve, reject) => {
-      axios({
-        method: requestType,
-        url: url,
-        data: data,
-        headers: headers,
+  return new Promise((resolve, reject) => {
+    axios(config)
+      .then((response) => {
+        Logger.log('HttpRequest response requestType = ' + requestType +
+        ' url   = ' + url + ' data = ', response);
+
+        // get token from response
+        // this.onSuccess(response.data);
+        resolve(response);
       })
-        .then((response) => {
-          const methodName = ' then(..) ';
-          Logger.log(this.className() + methodName +
-            'response = ', response);
-
-          // const token = this.getTokenFromResponse(response);
-          // if (token) {
-          //   TokenHandler.saveToken(token);
-          // }
-
-          this.onSuccess(response.data);
-          resolve(response);
-        })
-        .catch((error) => {
-          const methodName = ' catch(..) ';
-          Logger.log(this.className() + methodName + 'error = ', error);
-          this.onFail(error);
-
-          // TODO: this line is throwing an exception when response is undefined
-          // const status = error.response.status;
-
-          /* if (status === 401) {
+      .catch((error) => {
+        Logger.log('Http error = ', error);
+        // this.onFail(error);
+        // TODO: this line is throwing an exception when response is undefined
+        // const status = error.response.status;
+        /* if (status === 401) {
             // EventProvider.fire("unauthorized");
-          }
- */
-          reject(error);
-        });
-    });
-  }
-
-  /**
-   * retrieve the token from the response
-   * @param {*} response
-   * @return {string}
-   */
-  static getTokenFromResponse(response) {
-    const header = response.headers.authorization;
-    let token = null;
-    if (header) {
-      token =
-        header.split(' ')[1]; // discard the Bearer string in the header value
-    }
-    return token;
-  }
-
-  /**
-   * do something extra if the request was sucessful
-   * @param {Object} data
-   */
-  static onSuccess(data) {
-    // do something extra when the request is ok
-  }
-
-  /**
-   * do something extra if the request was invalid
-   * @param {Object} data
-   */
-  static onFail(data) {
-    // do something extra when the request have failed
-  }
+          } */
+        reject(error);
+      });
+  });
 }
+
+/**
+ * Send a GET request to the given URL.
+ *
+ * @param {string} url
+ * @return {Promise}
+ */
+function getRequest(url) {
+  return submit('get', url);
+}
+
+/**
+ * Send a POST request to the given URL.
+ *
+ * @param {string} url the request url
+ * @param {Object} data the data attached to the request
+ * @return {Promise}
+ */
+function postRequest(url, data={}) {
+  return submit('post', url, data);
+}
+
+/**
+ * Send a PUT request to the given URL.
+ *
+ * @param {string} url the request url
+ * @param {Object} data the data attached to the request
+ * @return {Promise}
+ */
+function putRequest(url, data={}) {
+  return submit('put', url, data);
+}
+
+/**
+ * Send a PATCH request to the given URL.
+ *
+ * @param {string} url the request url
+ * @param {Object} data the data attached to the request
+ * @return {Promise}
+ */
+function patchRequest(url, data={}) {
+  return submit('patch', url, data);
+}
+
+/**
+ * Send a DELETE request to the given URL.
+ *
+ * @param {string} url the request url
+ * @return {Promise}
+ */
+function deleteRequest(url) {
+  return submit('delete', url);
+};
