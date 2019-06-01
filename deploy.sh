c514044b7e096d6e1f78313635c7a8bfa77000bc
CMOW5 <tec.cmos@gmail.com>
2019-06-01 17:12:25 -0500

    deploy script added

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+git checkout build
+git merge master 
+npm run build
+git add .
+git commit -m "0.0.1 simple store"
+git subtree push --prefix build production master
+git checkout master
